677b8c957b5a1d0675040bd4f2152da10d8fc929
Chris Hammer <chris@thezengarden.net>
2015-03-10 09:43:45 -0400

    Logic for create/start/stop/destroy re-worked a bit. Testing.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,20 +12,21 @@ include_recipe "lxc_manage::packages"
 
 
 node["lxc_container"]["node"].each do |name,vars|
-  if vars['active'] and !vars['network']['hwaddr']
+  if vars['active'] && vars['run']
     lxc_manage_node "creating-#{name}" do
       lxc_name name
       lxc_ver  vars["lxc_version"] if vars["lxc_version"]
       lxc_vars vars
       action :create
+      not_if "lxc-ls | grep #{name}"
     end
-  elsif vars['active'] == true and vars['run'] == true
     lxc_manage_node "stop-#{name}" do
       lxc_name name
       action :start
       not_if "lxc-ls --active | grep #{name}"
+      only_if "lxc-ls | grep #{name}"
     end
-  elsif vars['active'] == true and vars['run'] == false
+  elsif vars['run'] == false
     lxc_manage_node "stop-#{name}" do
       lxc_name name
       action :stop

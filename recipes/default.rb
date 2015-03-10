66bb9005ea1b947df5fee87193bc6e0eea833649
Chris Hammer <chris@thezengarden.net>
2015-03-10 09:58:51 -0400

    Found issue with logic. Re-worked again. Testing.

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,25 +12,28 @@ include_recipe "lxc_manage::packages"
 
 
 node["lxc_container"]["node"].each do |name,vars|
-  if vars['active'] && vars['run']
-    lxc_manage_node "creating-#{name}" do
-      lxc_name name
-      lxc_ver  vars["lxc_version"] if vars["lxc_version"]
-      lxc_vars vars
-      action :create
-      not_if "lxc-ls | grep #{name}"
-    end
-    lxc_manage_node "start-#{name}" do
-      lxc_name name
-      action :start
-      not_if "lxc-ls --active | grep #{name}"
-      only_if "lxc-ls | grep #{name}"
-    end
-  elsif vars['run'] == false
-    lxc_manage_node "stop-#{name}" do
-      lxc_name name
-      action :stop
-      only_if "lxc-ls --active | grep #{name}"
+  if vars['active']
+    if vars['run'] == false
+      lxc_manage_node "stop-#{name}" do
+        lxc_name name
+        action :stop
+        only_if "lxc-ls | grep #{name}"
+        only_if "lxc-ls --active | grep #{name}"
+      end
+    else
+      lxc_manage_node "creating-#{name}" do
+        lxc_name name
+        lxc_ver  vars["lxc_version"] if vars["lxc_version"]
+        lxc_vars vars
+        action :create
+        not_if "lxc-ls | grep #{name}"
+      end
+      lxc_manage_node "start-#{name}" do
+        lxc_name name
+        action :start
+        not_if "lxc-ls --active | grep #{name}"
+        only_if "lxc-ls | grep #{name}"
+      end
     end
   elsif vars['active'] == false
     lxc_manage_node "destroying-#{name}" do

23716a9e96c335d34dbdf3c705e47bb05261ca39
Chris Hammer <chris@thezengarden.net>
2015-03-25 19:27:51 -0400

    Updated license for public release

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'lxc_manage'
-maintainer       'Copyright (C) 2015 The Zen Garden. All rights reserved.'
+maintainer       'Copyright (C) 2015 Chris Hammer'
 maintainer_email 'chris@thezengarden.net'
-license          'All rights reserved'
+license          'GPL v2'
 description      'Installs/Configures lxc_manage'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.4.10'

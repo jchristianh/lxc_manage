8970f89ee6b58ab8d6b9aac03d9a7e3fa81fe257
Chris Hammer <chris@thezengarden.net>
2015-03-31 13:44:24 -0400

    Added a CPU limit template option which is controlled via the data bag variables limit_cpu, cpu_cores, and cpu_shares

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'chris@thezengarden.net'
 license          'GPL v2'
 description      'Installs/Configures lxc_manage'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.10'
+version          '0.4.12'

ba543d69040224711661e04bea3bc5d494feaa32
Chris Hammer <chris@thezengarden.net>
2019-04-04 08:59:26 -0400

    0.4.4-DEV-190404-103149 -----------------------------     * FIX:       - node.set deprecated; see https://docs.chef.io/deprecations_attributes.html;         now using node.normal
        * MODIFY:
          - modified .gitlab-ci.yml to bring it up to date with our current ci process
          - modified metadata.rb to bring it up to date with best practices
    
        * REMOVED:
          - removed deprecated features: new_resource.updated_by_last_action, use_inline_resources
          - removed node.save as its no longer required with newer chef-client's

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
 name             'lxc_manage'
-maintainer       'Copyright (C) 2015 Chris Hammer'
+maintainer       'Copyright (C) 2019 Chris Hammer'
 maintainer_email 'chris@thezengarden.net'
-license          'GPL v2'
+license          'GPL-2.0'
 description      'Installs/Configures lxc_manage'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.3'
+version          '0.4.4'
 issues_url       'http://www.thezengarden.net'
 source_url       'http://www.thezengarden.net'
+chef_version     '>= 12.5' if respond_to?(:chef_version)
+supports         'centos', '>= 7.0'
+
